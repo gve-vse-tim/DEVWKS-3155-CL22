68a6a244bda88580333fe3fc8f7a69e43b17aa2f
Tim Miller <114172274+eti-tme-tim@users.noreply.github.com>
2025-04-09 14:46:14 -0400

    Update environment to latest versions - not tested

--- setup-python.sh
+++ setup-python.sh
@@ -4,7 +4,7 @@
 # work properly and be in a position to move forward.
 
 # Create Python Virtual Environment
-python3.10 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
 
 # Install Python required modules
@@ -14,10 +14,10 @@ pip install -r requirements.txt
 ansible-galaxy collection install -r requirements.yaml
 
 # Install Python utilities for NDFC
-pip install utils/dcnm_lan_fabric-0.2.4-py3-none-any.whl
+# pip install utils/dcnm_lan_fabric-0.2.4-py3-none-any.whl
 
 # Set up NDFCCTL connection credentials
-export NDFC_HOST=198.18.134.200
+export NDFC_HOST=198.18.134.100
 export NDFC_USER=admin
 export NDFC_PASS=C1sco12345
 
